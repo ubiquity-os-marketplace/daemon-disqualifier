83d1e7794823d168189a6ecb99d45d9a8b88b76c
gentlementlegen <fernand.veyrier@epitech.eu>
2024-06-03 15:45:45 +0900

    chore: fixed workflows

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,8 @@
 import { ValidationException } from "typebox-validators";
 import program from "./parser/payload";
-import { envConfigValidator } from "./types/env-type";
+import { EnvConfigType, envConfigValidator } from "./types/env-type";
 
-export async function run() {
+export async function run(env: EnvConfigType) {
   console.log(JSON.stringify(program, null, 2));
   if (!envConfigValidator.test(process.env)) {
     for (const error of envConfigValidator.errors(process.env)) {
@@ -11,7 +11,7 @@ export async function run() {
     return Promise.reject(new ValidationException("The environment is invalid."));
   }
   if (program.eventName === "issues") {
-    console.log("running");
+    console.log("running", env);
     return JSON.stringify({ status: "ok" });
   } else {
     console.warn(`${program.eventName} is not supported, skipping.`);
