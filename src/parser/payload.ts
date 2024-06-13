143f80d4ce66ce46ed43109b21518b76dd6248f8
gentlementlegen <fernand.veyrier@epitech.eu>
2024-06-14 01:58:00 +0900

    chore: decode for settings

--- src/parser/payload.ts
+++ src/parser/payload.ts
@@ -1,17 +1,20 @@
 import * as github from "@actions/github";
+import { Value } from "@sinclair/typebox/value";
 import { config } from "dotenv";
-import { PluginInputs } from "../types/plugin-inputs";
+import { PluginInputs, userActivityWatcherSettingsSchema } from "../types/plugin-inputs";
 
 config();
 
 const webhookPayload = github.context.payload.inputs;
+const settings = Value.Decode(userActivityWatcherSettingsSchema, Value.Default(userActivityWatcherSettingsSchema, JSON.parse(webhookPayload.settings)));
+
 const program: PluginInputs = {
   stateId: webhookPayload.stateId,
   eventName: webhookPayload.eventName,
   authToken: webhookPayload.authToken,
   ref: webhookPayload.ref,
   eventPayload: JSON.parse(webhookPayload.eventPayload),
-  settings: JSON.parse(webhookPayload.settings),
+  settings,
 };
 
 export default program;
