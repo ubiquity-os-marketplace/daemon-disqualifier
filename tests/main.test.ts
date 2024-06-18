c36f971ab9ee08dc66c88feab831575e010eebcc
gentlementlegen <fernand.veyrier@epitech.eu>
2024-06-18 10:31:35 +0200

    chore: added tests for singular and plural time durations

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -58,9 +58,14 @@ describe("Run tests", () => {
   });
   it("Should parse time", () => {
     expect(parseDurationString("Time: <1 Day").get("days")).toEqual(1);
+    expect(parseDurationString("Time: <1 Days").get("days")).toEqual(1);
     expect(parseDurationString("Time: <1 Week").get("weeks")).toEqual(1);
+    expect(parseDurationString("Time: <1 Weeks").get("weeks")).toEqual(1);
+    expect(parseDurationString("Time: <4 Hour").get("hours")).toEqual(4);
     expect(parseDurationString("Time: <4 Hours").get("hours")).toEqual(4);
+    expect(parseDurationString("Time: <8 Week").get("months")).toEqual(2);
     expect(parseDurationString("Time: <8 Weeks").get("months")).toEqual(2);
+    expect(parseDurationString("Time: <3 Month").get("months")).toEqual(3);
     expect(parseDurationString("Time: <3 Months").get("months")).toEqual(3);
   });
 });
