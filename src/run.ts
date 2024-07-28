1eac2e585108e27cc14652d7df6101c52b1619e9
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-28 22:41:48 +0100

    feat: ubiquibot-logger

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,8 @@
 import { Octokit } from "@octokit/rest";
 import { updateTasks } from "./helpers/update-tasks";
-import { proxyCallbacks } from "./proxy";
 import { Context } from "./types/context";
 import { PluginInputs } from "./types/plugin-inputs";
+import { Logs } from "@ubiquity-dao/ubiquibot-logger";
 
 export async function run(inputs: PluginInputs) {
   const octokit = new Octokit({ auth: inputs.authToken });
@@ -11,25 +11,9 @@ export async function run(inputs: PluginInputs) {
     payload: inputs.eventPayload,
     config: inputs.settings,
     octokit,
-    logger: {
-      debug(message: unknown, ...optionalParams: unknown[]) {
-        console.debug(message, ...optionalParams);
-      },
-      info(message: unknown, ...optionalParams: unknown[]) {
-        console.log(message, ...optionalParams);
-      },
-      warn(message: unknown, ...optionalParams: unknown[]) {
-        console.warn(message, ...optionalParams);
-      },
-      error(message: unknown, ...optionalParams: unknown[]) {
-        console.error(message, ...optionalParams);
-      },
-      fatal(message: unknown, ...optionalParams: unknown[]) {
-        console.error(message, ...optionalParams);
-      },
-    },
+    logger: new Logs("info"),
   };
 
   await updateTasks(context);
-  return JSON.stringify(await proxyCallbacks[inputs.eventName](context));
+  return JSON.stringify({ status: "ok" }); // don't really need to return anything to the kernel
 }
