1d3dabc70c14d144ed60c5ce1ffb0eb64b1e1a64
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-08-05 20:37:05 +0100

    refactor: remove worker logic

--- src/helpers/github-url.ts
+++ src/helpers/github-url.ts
@@ -9,25 +9,3 @@ export function parseIssueUrl(url: string): { owner: string; repo: string; issue
     issue_number: Number(path[4]),
   };
 }
-
-export function parseRepoUrl(repoUrl: string) {
-  if (!repoUrl) {
-    throw new Error(`[parseRepoUrl] Missing repo URL`);
-  }
-  const urlObject = new URL(repoUrl);
-  const urlPath = urlObject.pathname.split("/");
-
-  if (urlPath.length === 3) {
-    const ownerName = urlPath[1];
-    const repoName = urlPath[2];
-    if (!ownerName || !repoName) {
-      throw new Error(`Missing owner name or repo name in [${repoUrl}]`);
-    }
-    return {
-      owner: ownerName,
-      repo: repoName,
-    };
-  } else {
-    throw new Error(`[parseRepoUrl] Invalid repo URL: [${repoUrl}]`);
-  }
-}
