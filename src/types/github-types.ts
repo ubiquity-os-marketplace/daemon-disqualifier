615728782f6a73890431584b592ffc2b184a83b5
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-15 19:13:47 +0100

    chore: knip

--- src/types/github-types.ts
+++ src/types/github-types.ts
@@ -3,5 +3,4 @@ import { RestEndpointMethodTypes } from "@octokit/rest";
 export type ListForOrg = RestEndpointMethodTypes["repos"]["listForOrg"]["response"];
 export type ListIssueForRepo = RestEndpointMethodTypes["issues"]["listForRepo"]["response"]["data"][0];
 export type ListCommentsForIssue = RestEndpointMethodTypes["issues"]["listComments"]["response"]["data"][0];
-export type GitHubListEvents = RestEndpointMethodTypes["issues"]["listEvents"]["response"]["data"][0];
 export type GitHubTimelineEvents = RestEndpointMethodTypes["issues"]["listEventsForTimeline"]["response"]["data"][0];
