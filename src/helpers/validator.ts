6ac650ae24e18450a014c457a4f6513299889106
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-02 16:32:39 +0900

    chore: changed config generation

--- src/helpers/validator.ts
+++ src/helpers/validator.ts
@@ -9,7 +9,6 @@ export function validateAndDecodeSchemas(rawEnv: object, rawSettings: object) {
   const env = Value.Default(envSchema, rawEnv) as Env;
   if (!envValidator.test(env)) {
     for (const error of envValidator.errors(env)) {
-      console.error(error);
       errors.push(error);
     }
   }
@@ -17,7 +16,6 @@ export function validateAndDecodeSchemas(rawEnv: object, rawSettings: object) {
   const settings = Value.Default(pluginSettingsSchema, rawSettings) as PluginSettings;
   if (!pluginSettingsValidator.test(settings)) {
     for (const error of pluginSettingsValidator.errors(settings)) {
-      console.error(error);
       errors.push(error);
     }
   }
@@ -31,7 +29,6 @@ export function validateAndDecodeSchemas(rawEnv: object, rawSettings: object) {
     const decodedEnv = Value.Decode(envSchema, rawEnv || {});
     return { decodedEnv, decodedSettings };
   } catch (e) {
-    console.error("validateAndDecodeSchemas", e);
     if (e instanceof TransformDecodeCheckError || e instanceof TransformDecodeError) {
       throw { errors: [e.error] };
     }
