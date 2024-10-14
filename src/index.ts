0dc51b35e533f3070b83e04644b9bc02ecf11a10
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-14 12:32:20 +0900

    chore: test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-// import * as core from "@actions/core";
-// import program from "./parser/payload";
-// import { run } from "./run";
-//
-// run(program)
-//   .then((result) => core?.setOutput("result", result))
-//   .catch((e) => {
-//     console.error("Failed to run user-activity-watcher:", e);
-//     core?.setFailed(e.toString());
-//   });
+import * as core from "@actions/core";
+import program from "./parser/payload";
+import { run } from "./run";
+
 console.log("hello!");
+
+run(program)
+  .then((result) => {
+    console.log("Done.");
+    return core?.setOutput("result", result);
+  })
+  .catch((e) => {
+    console.error("Failed to run user-activity-watcher:", e);
+    core?.setFailed(e.toString());
+  });
