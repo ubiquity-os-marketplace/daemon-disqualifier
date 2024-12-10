c7a78817ef4f7418996bc4e574bcb16d00676bd9
gentlementlegen <fernand.veyrier@epitech.eu>
2024-12-10 13:48:23 +0900

    chore: make sure that we comment to the main issue if no pull-request is present

--- src/helpers/remind-and-remove.ts
+++ src/helpers/remind-and-remove.ts
@@ -57,8 +57,10 @@ async function remindAssignees(context: ContextPlugin, issue: ListIssueForRepo) 
       body: [logMessage.logMessage.raw, metadata].join("\n"),
     });
   } else {
-    const pullRequests = await collectLinkedPullRequests(context, { repo, owner, issue_number });
-    let shouldPostToMainIssue = false;
+    const pullRequests = (await collectLinkedPullRequests(context, { repo, owner, issue_number }))
+      // We filter out closed and merged PRs to avoid commenting on these
+      .filter((o) => o.state === "OPEN");
+    let shouldPostToMainIssue = pullRequests.length === 0;
     for (const pullRequest of pullRequests) {
       const { owner: prOwner, repo: prRepo, issue_number: prNumber } = parseIssueUrl(pullRequest.url);
       try {
