f7ca9e0b11d1f02f3f3a57f552386f5a48817cfc
gentlementlegen <fernand.veyrier@epitech.eu>
2024-12-11 11:53:07 +0900

    chore: downgraded typebox version

--- src/worker.ts
+++ src/worker.ts
@@ -15,7 +15,7 @@ const app = createPlugin<PluginSettings, Env, null, SupportedEvents>(
     settingsSchema: pluginSettingsSchema,
     logLevel: process.env.LOG_LEVEL || LOG_LEVEL.INFO,
     postCommentOnError: false,
-    ...(process.env.KERNEL_PUBLIC_KEY && { kernelPublicKey: process.env.KERNEL_PUBLIC_KEY }),
+    kernelPublicKey: process.env.KERNEL_PUBLIC_KEY,
     bypassSignatureVerification: process.env.NODE_ENV === "local",
   }
 );
