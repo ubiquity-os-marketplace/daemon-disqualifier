c386cfd239ee661ef8192538a7afe3da425970b7
gentlementlegen <fernand.veyrier@epitech.eu>
2025-01-07 21:37:31 +0900

    fix: the warning for the disqualification is not posted on tasks with no price label

--- tests/assign.test.ts
+++ tests/assign.test.ts
@@ -1,4 +1,4 @@
-import { jest } from "@jest/globals";
+import { beforeEach, describe, expect, it, jest } from "@jest/globals";
 import { Logs } from "@ubiquity-os/ubiquity-os-logger";
 import { ContextPlugin } from "../src/types/plugin-input";
 
@@ -13,7 +13,7 @@ jest.unstable_mockModule("@ubiquity-os/plugin-sdk", () => ({
 const { watchUserActivity } = await import("../src/handlers/watch-user-activity");
 
 describe("watchUserActivity", () => {
-  const mockContext = {
+  const mockContextTemplate = {
     logger: new Logs("debug"),
     eventName: "issues.assigned",
     payload: {
@@ -46,14 +46,34 @@ describe("watchUserActivity", () => {
     jest.clearAllMocks();
   });
 
-  test("posts comment for matching repository issue", async () => {
+  it("should post comment for matching repository issue", async () => {
     const spy = jest.spyOn(console, "warn");
-    await watchUserActivity(mockContext);
+    const mockContext = {
+      ...mockContextTemplate,
+    };
+    mockContext.payload = {
+      ...mockContextTemplate.payload,
+      issue: {
+        assignees: [{ login: "ubiquity-os" }],
+        title: "Test Issue",
+        state: "open",
+        labels: ["Price: 1 USD"],
+      },
+    } as unknown as ContextPlugin["payload"];
 
+    await watchUserActivity(mockContext);
     expect(spy).toHaveBeenNthCalledWith(
       1,
       expect.stringMatching(/(Reminders will be sent every `1 hour` if there is no activity\.|Assignees will be disqualified after `2 hours` of inactivity\.)/)
     );
+    spy.mockReset();
+  });
+
+  it("should ignore an un-priced task", async () => {
+    const spy = jest.spyOn(console, "info");
+
+    await watchUserActivity(mockContextTemplate);
+    expect(spy).not.toHaveBeenCalled();
     spy.mockReset();
   });
 });
