f1d5b2b54cc69e72268066ec7a60c4cdd2238891
gentlementlegen <fernand.veyrier@epitech.eu>
2025-01-21 18:18:06 +0900

    chore: changed cron job time

--- src/run.ts
+++ src/run.ts
@@ -3,5 +3,6 @@ import { ContextPlugin } from "./types/plugin-input";
 
 export async function run(context: ContextPlugin) {
   context.logger.debug("Will run with the following configuration:", { configuration: context.config });
+  console.log(JSON.stringify(context, null, 2));
   return watchUserActivity(context);
 }
