e46cd0a9c522ba73f7fc6de1efca39c19454b97f
gentlementlegen <fernand.veyrier@epitech.eu>
2025-02-03 15:45:05 +0900

    fix: proper octokit credentials are used based on the repository

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -1,4 +1,6 @@
+import { createAppAuth } from "@octokit/auth-app";
 import { Octokit } from "@octokit/rest";
+import { customOctokit } from "@ubiquity-os/plugin-sdk/dist/octokit";
 import { Logs } from "@ubiquity-os/ubiquity-os-logger";
 import db from "./database-handler";
 
@@ -7,6 +9,7 @@ async function main() {
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
   });
+
   const fileContent = db.data;
   for (const [key, value] of Object.entries(fileContent)) {
     try {
@@ -15,14 +18,26 @@ async function main() {
         value,
       });
       const [owner, repo] = key.split("/");
+      const installation = await octokit.rest.apps.getRepoInstallation({
+        owner,
+        repo,
+      });
       const comment = value.pop();
       if (!comment) {
         logger.error(`No comment was found for repository ${key}`);
         continue;
       }
+      const repoOctokit = new customOctokit({
+        authStrategy: createAppAuth,
+        auth: {
+          appId: Number(process.env.APP_ID),
+          privateKey: process.env.APP_PRIVATE_KEY,
+          installationId: installation.data.id,
+        },
+      });
       const {
         data: { body = "" },
-      } = await octokit.rest.issues.getComment({
+      } = await repoOctokit.rest.issues.getComment({
         owner,
         repo,
         comment_id: comment.commentId,
@@ -30,7 +45,7 @@ async function main() {
       });
       const newBody = body + `\n<!-- daemon-disqualifier update ${Date().toLocaleString()} -->`;
       logger.debug(`Update comment ${comment.commentId}`, { newBody });
-      await octokit.rest.issues.updateComment({
+      await repoOctokit.rest.issues.updateComment({
         owner,
         repo,
         comment_id: comment.commentId,
